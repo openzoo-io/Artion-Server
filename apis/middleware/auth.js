ab054f759eb719c3d267725a7ebfca73db5d03f3
lolieatapple <lolieatapple@gmail.com>
2021-11-04 23:36:24 +0800

    no auth

--- apis/middleware/auth.js
+++ apis/middleware/auth.js
@@ -7,19 +7,20 @@ const auth = (req, res, next) => {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null)
-      return res.status(401).json({
-        status: "failed",
-        data: "auth token not provided",
-      });
-    jwt.verify(token, jwt_secret, (err) => {
-      if (err)
-        return res.status(400).json({
-          status: "failed",
-          data: "auth token expired",
-        });
-      next();
-    });
+    next();
+    // if (token == null)
+    //   return res.status(401).json({
+    //     status: "failed",
+    //     data: "auth token not provided",
+    //   });
+    // jwt.verify(token, jwt_secret, (err) => {
+    //   if (err)
+    //     return res.status(400).json({
+    //       status: "failed",
+    //       data: "auth token expired",
+    //     });
+    //   next();
+    // });
   } catch (error) {
     return res.status(400).json({
       status: "failed",
