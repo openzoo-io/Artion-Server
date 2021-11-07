04ec0cc3c288b61970ea4430830c0770b9f3f779
lolieatapple <lolieatapple@gmail.com>
2021-11-07 23:45:54 +0800

    update

--- constants/tokens.js
+++ constants/tokens.js
@@ -16,7 +16,7 @@ const DISABLED_PAYTOKENS = process.env.NETWORK_CHAINID === "250" ? [
   ]
 
 
-const PAYTOKENS = process.env.NETWORK_CHAINID === "999" ? [
+const PAYTOKENS = process.env.NETWORK_CHAINID === "888" ? [
     {
       address: '0xdabd997ae5e4799be47d6e69d9431615cba28f48',
       name: 'Wrapped Wanchain',
