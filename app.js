ad6e320ca49ca2066dd3c9c9948c104f42b16125
moohooooo <suwichalala@gmail.com>
2021-12-09 00:49:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ require("./models/unlockable");
 require("./models/disabledExplorerCollection");
 
 app.use(bodyParser.json({limit: '50mb'}));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(
   cors()
 );
