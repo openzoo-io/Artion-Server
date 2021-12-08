fe9f1f617300ab0a6a73ee0d5ec19abf6f41c102
moohooooo <suwichalala@gmail.com>
2021-12-09 00:59:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5001;
 
 const Logger = require('./services/logger');
 const morganMiddleware = require('./apis/middleware/morgan');
-app.use(express.limit('4M'));
+
 app.use(
   bodyParser.urlencoded({
     extended: false,
