1280043425a39cbf0051c5c7d511486f6229998b
moohooooo <suwichalala@gmail.com>
2021-12-09 02:30:33 +0700

    Update app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ require("./models/disabledExplorerCollection");
 
 app.use(bodyParser.json({limit: '50mb'})); 
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 app.use(
   cors()
 );
