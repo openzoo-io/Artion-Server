e6eb72df617b572c541e04dfdf2e8013c5e6a46f
moohooooo <suwichalala@gmail.com>
2021-12-16 13:40:59 +0700

    Update adminAddress.js

--- config/adminAddress.js
+++ config/adminAddress.js
@@ -1,3 +1,3 @@
-const AdminAddresses = ["0x43870be251d688f5e474d77fca65709fe8d1f03b",'0x4cf0a877e906dead748a41ae7da8c220e4247d9e'];
+const AdminAddresses = ["0x43870be251d688f5e474d77fca65709fe8d1f03b",'0x4cf0a877e906dead748a41ae7da8c220e4247d9e','"0x6ab499c8e2f3cbc9c99034b6e2912149212be770"'];
 
 module.exports = AdminAddresses;
