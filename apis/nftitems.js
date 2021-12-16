2a911b2aca575386dd86be19d0f6cfab8941c20e
moohooooo <suwichalala@gmail.com>
2021-12-16 21:23:39 +0700

    Update nftitems.js

--- apis/nftitems.js
+++ apis/nftitems.js
@@ -439,7 +439,7 @@ const selectTokens = async (req, res) => {
           thumbnailPath: { $ne: nonImage },
           isAppropriate: true
         };
-        const tokens_721 = NFTITEM.find(collectionFilters721).select(selectOption).lean();
+        const tokens_721 = NFTITEM.find(collectionFilters721).select(selectOption);
         //return tokens_721;
         // TODO enable erc1155
         let collectionFilters1155 = {
@@ -486,7 +486,7 @@ const selectTokens = async (req, res) => {
             });
         });
         
-        let allTokens = [ ...tokens_1155];
+        let allTokens = [...tokens_721, ...tokens_1155];
         console.log(allTokens);
         return allTokens
       }
