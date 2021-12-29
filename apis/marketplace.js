9883d3f806f82676f83ebd09747c3349b19aa71c
moohooooo <suwichalala@gmail.com>
2021-12-29 15:21:58 +0700

    Update marketplace.js

--- apis/marketplace.js
+++ apis/marketplace.js
@@ -168,7 +168,7 @@ router.post("/itemSold", service_auth, async (req, res) => {
     const quantity = parseInt(quantityBN.hex);
 
     const priceInUSD = pricePerItem * getPrice(itemPayToken.address);
-    
+
     // update last sale price
     // first update the token price
     let category = await Category.findOne({ minterAddress: nft });
@@ -268,7 +268,7 @@ router.post("/itemSold", service_auth, async (req, res) => {
       const listing = await Listing.findOne({ owner: seller, minter: nft, tokenID: tokenId });
       //console.log('quantity', listing.quantity);
       if (listing && quantity >= listing.quantity) {
-        console.log('1155 Sold exactly amount case');
+        console.log('1155 / 721 Sold exactly amount case');
         // remove from listing
         await Listing.deleteMany({
           owner: seller,
@@ -309,54 +309,35 @@ router.post("/itemSold", service_auth, async (req, res) => {
 
       }
       else {
-        if (listing.quantity) { // 1155
-          console.log('1155 Sold reduce case');
-          // reduce listing
-          await Listing.updateOne({
-            owner: seller,
-            minter: nft,
-            tokenID: tokenId,
-            blockNumber: { $lte: blockNumber }
-          }, {
-            $inc: { "quantity": (quantity * -1) }
-          });
-          if (token) {
-            token.price = pricePerItem;
-            token.paymentToken = itemPayToken.address;
-            token.priceInUSD = priceInUSD;
-            token.lastSalePrice = pricePerItem;
-            token.lastSalePricePaymentToken = itemPayToken.address;
-            token.lastSalePriceInUSD = priceInUSD;
-            token.listedAt = new Date(0);
-            token.soldAt = new Date(); //set recently sold date
-            await token.save();
-          }
-        }
-        else // 721
-        {
-          console.log('721 Sold case');
-          if (token) {
-            token.price = 0;
-            token.paymentToken = "wan";
-            token.priceInUSD = 0;
-            token.lastSalePrice = pricePerItem;
-            token.lastSalePricePaymentToken = itemPayToken.address;
-            token.lastSalePriceInUSD = priceInUSD;
-            token.listedAt = new Date(0);
-            token.soldAt = new Date(); //set recently sold date
-            token.owner = buyer;
-            await token.save();
-          }
+
+        console.log('1155 Sold reduce case');
+        // reduce listing
+        await Listing.updateOne({
+          owner: seller,
+          minter: nft,
+          tokenID: tokenId,
+          blockNumber: { $lte: blockNumber }
+        }, {
+          $inc: { "quantity": (quantity * -1) }
+        });
+        if (token) {
+          token.price = pricePerItem;
+          token.paymentToken = itemPayToken.address;
+          token.priceInUSD = priceInUSD;
+          token.lastSalePrice = pricePerItem;
+          token.lastSalePricePaymentToken = itemPayToken.address;
+          token.lastSalePriceInUSD = priceInUSD;
+          token.listedAt = new Date(0);
+          token.soldAt = new Date(); //set recently sold date
+          await token.save();
         }
       }
+
 
     } catch (err) {
       Logger.error("[ItemSold] Failed to udpate date listing: ", err.message);
       console.log(err.message);
     }
-
-
-
 
 
     Logger.info("[ItemSold] Success: ", { transactionHash, blockNumber });
