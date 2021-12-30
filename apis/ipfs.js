9dd3aa6686f44e34a171669f2f94b81d1de2a7da
lolieatapple <lolieatapple@gmail.com>
2021-12-30 13:57:38 +0800

    update

--- apis/ipfs.js
+++ apis/ipfs.js
@@ -16,6 +16,7 @@ const pinataSDK = require("@pinata/sdk");
 const toLowerCase = require("../utils/utils");
 
 const extractAddress = require("../services/address.utils");
+const service_auth = require("./middleware/auth.tracker");
 
 // const ipfsUris = ["https://artion.mypinata.cloud/ipfs/", "https://artion1.mypinata.cloud/ipfs/", "https://artion2.mypinata.cloud/ipfs/", "https://artion3.mypinata.cloud/ipfs/", "https://artion4.mypinata.cloud/ipfs/", "https://artion5.mypinata.cloud/ipfs/", "https://artion6.mypinata.cloud/ipfs/", "https://artion7.mypinata.cloud/ipfs/", "https://artion8.mypinata.cloud/ipfs/", "https://artion9.mypinata.cloud/ipfs/", "https://artion10.mypinata.cloud/ipfs/", "https://artion11.mypinata.cloud/ipfs/", "https://artion12.mypinata.cloud/ipfs/", "https://artion13.mypinata.cloud/ipfs/"];
 const ipfsUris = ["https://openzoo.mypinata.cloud/ipfs/"];
@@ -213,7 +214,7 @@ router.get("/ipfstest", async (req, res) => {
       });
     });
 });
-router.get("/test", auth, async (req, res) => {
+router.get("/test", service_auth, async (req, res) => {
   return res.json({
     apistatus: "running",
   });
