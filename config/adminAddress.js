cc0b0f960a39c2035f9a1901edd113b5f81913ef
lolieatapple <lolieatapple@gmail.com>
2021-12-30 13:27:36 +0800

    update admin address for mainnet

--- config/adminAddress.js
+++ config/adminAddress.js
@@ -1,3 +1,3 @@
-const AdminAddresses = ["0x43870be251d688f5e474d77fca65709fe8d1f03b",'0x4cf0a877e906dead748a41ae7da8c220e4247d9e'];
+const AdminAddresses = ["0x43870be251d688f5e474d77fca65709fe8d1f03b",'0xa63c637e369021f737b3bcf06a5743d6c2ddab6c'];
 
 module.exports = AdminAddresses;
