ea72c19582324829103af6d0ae8153fe98ed05f1
moohooooo <suwichalala@gmail.com>
2022-02-02 14:09:31 +0700

    Update nftitems.js

--- apis/nftitems.js
+++ apis/nftitems.js
@@ -133,7 +133,7 @@ router.post('/resyncMetajson', async (req, res) => {
     });
     if (token) {
       const SimplifiedERC721ABI = require('../constants/simplifiederc721abi');
-      let sc = ethers.Contract(contractAddress, SimplifiedERC721ABI, provider);
+      let sc = new ethers.Contract(contractAddress, SimplifiedERC721ABI, provider);
       let tokenURI = await sc.tokenURI(tokenID);
       let metadata;
 
