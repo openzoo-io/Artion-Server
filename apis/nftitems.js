58d3e13e9a2d57e281027a8313e138c752ebab45
moohooooo <suwichalala@gmail.com>
2023-01-17 14:32:52 +0700

    Update nftitems.js

--- apis/nftitems.js
+++ apis/nftitems.js
@@ -29,7 +29,7 @@ const sortBy = require('lodash.sortby');
 const Logger = require('../services/logger');
 const { MAX_INTEGER } = require('ethereumjs-util');
 
-var crypto = require('crypto');
+
 const NodeCache = require("node-cache");
 const myCache = new NodeCache({ stdTTL: 15, checkperiod: 0 });
 
@@ -561,12 +561,11 @@ const selectTokens = async (req, res) => {
           ...(mediaType ? { contentType: mediaType } : {})
         };
 
-        let key = crypto.createHash('md5').update(JSON.stringify(collectionFilters)).digest('hex');
-        let nftListCache = myCache.get(key);
+        let nftListCache = myCache.get(JSON.stringify(collectionFilters));
         if (nftListCache === undefined) {
           let ret = NFTITEM.find(collectionFilters).select(selectOption).lean();
 
-          myCache.set(key, ret);
+          myCache.set(JSON.stringify(collectionFilters), ret);
         }
 
         return nftListCache;
