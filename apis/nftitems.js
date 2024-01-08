c2564c57e6db409da343dc2b393a67f6890e94b2
CryptoFennec <suwichalala@gmail.com>
2024-01-08 21:43:21 +0700

    Update nftitems.js

--- apis/nftitems.js
+++ apis/nftitems.js
@@ -556,9 +556,9 @@ const selectTokens = async (req, res) => {
           ...(collections2filter === null
             ? {}
             : { contractAddress: { $in: [...collections2filter] } }),
-          //thumbnailPath: { $ne: nonImage },
+          thumbnailPath: { $ne: nonImage },
           isAppropriate: true,
-          //...(mediaType ? { contentType: mediaType } : {})
+          ...(mediaType ? { contentType: mediaType } : {})
         };
        // let nftListCache = myCache.get(JSON.stringify(collectionFilters));
        // console.log('nftListCache',nftListCache)
